ffaf318bca7a76310f03167e2798790afa77fbe0
fabian4 <fabian4@163.com>
2021-11-15 16:06:10 +0800

    update repo

--- src/help/message.ts
+++ src/help/message.ts
@@ -3,7 +3,7 @@ import {api} from "./Api";
 import stringRandom from "string-random";
 import {config} from "../config";
 
-export function send(to: string, msg: string){
+export function send(to: string, msg: string) {
     post(api.sendMessage, {
         "messageId": stringRandom(20),
         "messageList": [
@@ -24,4 +24,97 @@ export function send(to: string, msg: string){
         "conversationId": "XSFDSFDFSAFDSAS^%",
         "contributionId": "SFF$#REGFY7&^%THT"
     })
+
+    // post(api.sendMessage, {
+    //         "messageId": stringRandom(20),
+    //         "messageList": [
+    //             {
+    //                 "contentType": "text/plain",
+    //                 "contentText": msg
+    //             },
+    //             {
+    //                 "contentType": "application/vnd.gsma.botsuggestion.v1.0+json",
+    //                 "contentText": {
+    //                     "suggestions": [
+    //                         {
+    //                             "reply": {
+    //                                 "displayText": "Yes",
+    //                                 "postback": {
+    //                                     "data": "set_by_chatbot_reply_yes"
+    //                                 }
+    //                             }
+    //                         },
+    //                         {
+    //                             "reply": {
+    //                                 "displayText": "No",
+    //                                 "postback": {
+    //                                     "data": "set_by_chatbot_reply_no"
+    //                                 }
+    //                             }
+    //                         },
+    //                         {
+    //                             "action": {
+    //                                 "urlAction": {
+    //                                     "openUrl": {
+    //                                         "url": "https://www.10010.com"
+    //                                     }
+    //                                 },
+    //                                 "displayText": "Open website or deep link",
+    //                                 "postback": {
+    //                                     "data": "set_by_chatbot_open_url"
+    //                                 }
+    //                             }
+    //                         },
+    //                         {
+    //                             "action": {
+    //                                 "dialerAction": {
+    //                                     "dialPhoneNumber": {
+    //                                         "phoneNumber": "+8617928222350"
+    //                                     }
+    //                                 },
+    //                                 "displayText": "Call a phone number",
+    //                                 "postback": {
+    //                                     "data": "set_by_chatbot_open_dialer"
+    //                                 }
+    //                             }
+    //                         }
+    //                     ]
+    //                 }
+    //             }
+    //         ],
+    //         "destinationAddress": [
+    //             `tel:+86${to}`
+    //         ],
+    //         "smsSupported": false,
+    //         "storeSupported": false,
+    //         "senderAddress": config.chatbotId,
+    //         "serviceCapability": [
+    //             {
+    //                 "capabilityId": "ChatbotSA",
+    //                 "version": "+g.gsma.rcs.botversion=\"#=1\""
+    //             }
+    //         ],
+    //         "conversationId": "XSFDSFDFSAFDSAS^%",
+    //         "contributionId": "SFF$#REGFY7&^%THT"
+    //     }
+    // )
+
+
+    // post(api.sendMessage, {
+    //         "messageId": stringRandom(20),
+    //         "messageList": [
+    //             {
+    //                 "contentType": "text/plain",
+    //                 "contentText": "geo:50.7311865,7.0914591;crs=gcj02;u=10;rcs-l=Qingfeng%20Steamed%20Dumpling%20Shop %20%F0%9F%8D%9A"
+    //             }
+    //         ],
+    //         "destinationAddress": [`tel:+86${to}`],
+    //         "smsSupported": false,
+    //         "storeSupported": false,
+    //         "senderAddress": config.chatbotId,
+    //         "serviceCapability": [{"capabilityId": "ChatbotSA", "version": "+g.gsma.rcs.botversion=\"#=1\""}],
+    //         "conversationId": "XSFDSFDFSAFDSAS^%",
+    //         "contributionId": "SFF$#REGFY7&^%THT"
+    //     }
+    // )
 }
