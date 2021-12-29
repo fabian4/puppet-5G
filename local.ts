2f14e5f9b8cf034195adfddbc32488471c3009d4
fabian4 <fabian4@163.com>
2021-12-29 08:51:12 +0800

    feat(message): 消息撤回

--- local.ts
+++ local.ts
@@ -1,5 +1,5 @@
 export const local = {
-    sipId: '20210401',
-    appId: '28871d8c83954bc78424ffcbff80285c',
-    appKey: '3b9cc5506af2466aa82eee4c04f86471',
+    sipId: 'xxxxx',
+    appId: 'xxxxxxxxxxxxxx',
+    appKey: 'xxxxxxxxxxxxxxx',
 }
